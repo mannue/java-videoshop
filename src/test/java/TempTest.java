db0e37b4ce6b5134358b371dd4a7dc0c61da4cb7
junyoungpark92 <junyoung.park@meshkorea.net>
2019-04-22 17:19:13 +0900

    init project

---
+++ src/test/java/TempTest.java
@@ -0,0 +1,10 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class TempTest {
+    @Test
+    public void testSomething() {
+        assertThat("test").isEqualTo("test");
+    }
+}
